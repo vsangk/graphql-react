be5174409e0d56dc19a60089c26e251c4cf42ed0
Vince Sangkhae <vsangk@gmail.com>
2019-11-02 00:04:01 -0700

    Add Item datamodel

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,19 @@
-const mutations = {};
+const mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+
+    // returning a promise works here too
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+
+    return item;
+  },
+};
 
 module.exports = mutations;
