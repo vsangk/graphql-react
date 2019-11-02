58a7bb33598fc42656e5047e3617e56573b65ebd
Vince Sangkhae <vsangk@gmail.com>
2019-11-01 22:43:48 -0700

    Init yoga server

---
+++ backend/src/db.js
@@ -0,0 +1,12 @@
+// This file connects to the remote prisma server and feeds it
+// the possible mutations and queries
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false,
+});
+
+module.exports = db;
