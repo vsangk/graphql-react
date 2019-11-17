6a015cb0f99f8d9f61385ad9930c84251725ac9b
Vince Sangkhae <vsangk@gmail.com>
2019-11-17 10:18:32 -0800

    Implement item deletion

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -29,6 +29,16 @@ const mutations = {
       info
     );
   },
+  async deleteItem(parent, args, ctx, info) {
+    console.log(parent, args, ctx, info);
+    const where = { id: args.id };
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{ id title }`);
+    // 2. check if they own that item or have the permissions
+    // TODO
+    // 3. delete it
+    return ctx.db.mutation.deleteItem({ where }, info);
+  },
 };
 
 module.exports = mutations;
--- frontend/components/UpdateItem.js
+++ frontend/components/UpdateItem.js
@@ -49,8 +49,6 @@ class UpdateItem extends Component {
 
   updateItem = async (e, updateItemMutation) => {
     e.preventDefault();
-    console.log('Updating Item');
-    console.log(this.state);
     const res = await updateItemMutation({
       variables: { id: this.props.id, ...this.state },
     });
