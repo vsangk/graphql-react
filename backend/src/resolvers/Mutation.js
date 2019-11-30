3ec5ecde40563f31df5a2737b690de4674b8f06e
Vince Sangkhae <vsangk@gmail.com>
2019-11-30 14:10:55 -0800

    Implement backend user creation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check if they are logged in
@@ -38,6 +41,26 @@ const mutations = {
     // TODO
     // 3. delete it
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parents, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+    const password = await bcrypt.hash(args.password, 10);
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] },
+        },
+      },
+      info
+    );
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365,
+    });
+    return user;
   },
 };
 
